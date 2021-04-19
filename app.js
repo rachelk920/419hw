fd3931d15f244b86f9f523c9fc64265338496093
Rachel <rmshinski@gmail.com>
2021-04-19 17:01:09 -0400

    hw

---
+++ app.js
@@ -0,0 +1,24 @@
+
+var myArray = [];
+for (var i = 1; i < 11; i++) {
+  myArray.push(i);
+}
+
+
+
+var myArray1 = [];
+var i = 10;
+while(i >= 1) {
+    myArray1.push(i);
+    i--;
+}
+
+var myArray2 = [];
+for (var i = 11; i < 22; i += 2){
+    myArray2.push(i);
+}
+
+var myArray3 = [];
+for (var i = 15; i > 0; i -= 2) {
+    myArray3.push(i);
+}
